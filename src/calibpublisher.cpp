27e04e44f686012aa75885abf076c7241f97ce04
jacob <jhultman0@gmail.com>
2019-03-15 22:52:24 -0400

    add calib img decoding upon subscriber receipt

--- src/calibpublisher.cpp
+++ src/calibpublisher.cpp
@@ -11,8 +11,20 @@
 #include <opencv2/opencv.hpp>
 #include "std_msgs/String.h"
 
-void callback(const sensor_msgs::ImageConstPtr& msg) {
+void callback(const sensor_msgs::ImageConstPtr& msg)
+{
     ROS_INFO("Received calib matrix.");
+    cv_bridge::CvImagePtr cv_ptr;
+    try
+    {
+        cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::TYPE_32FC1);
+        std::cout << cv_ptr->image << std::endl;
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_ERROR("cv_bridge failure upon calib receipt: %s", e.what());
+        return;
+    }
 }
 
 int main(int argc, char **argv)
@@ -21,8 +33,8 @@ int main(int argc, char **argv)
     ros::NodeHandle nh;
     image_transport::ImageTransport xport(nh);
 
-    image_transport::Publisher publisher = xport.advertise("calibration", 1);
-    image_transport::Subscriber subscriber = xport.subscribe("calibration", 1, callback);
+    image_transport::Publisher publisher = xport.advertise("/calibration/velotoimage", 1);
+    image_transport::Subscriber subscriber = xport.subscribe("/calibration/velotoimage", 1, callback);
 
     std::string basedir = argv[1];
     Calibration calib = KittiReader::makeCalib(basedir);
@@ -32,7 +44,7 @@ int main(int argc, char **argv)
     calibMatrix.encoding = sensor_msgs::image_encodings::TYPE_32FC1;
     calibMatrix.image = mat;
 
-    ros::Rate loop_rate(1);
+    ros::Rate loop_rate(0.2);
     int count = 0;
     while (ros::ok())
     {
