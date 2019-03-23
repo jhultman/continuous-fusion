d4d1bdc47a0af27f1c80ee4f69604a3c74861752
jacob <jhultman0@gmail.com>
2019-03-23 18:01:38 -0400

    refactor velo publisher

--- src/velopublisher.cpp
+++ src/velopublisher.cpp
@@ -1,27 +1,37 @@
+#include "velopublisher.hpp"
 #include "kittireader.hpp"
+#include <opencv2/opencv.hpp>
 #include "ros/ros.h"
 #include <pcl_ros/point_cloud.h>
 #include <pcl/point_types.h>
 
+VeloPublisher::VeloPublisher()
+{
+    _publisher = _nh.advertise<pcl::PointCloud<pcl::PointXYZI>>("/velodyne/points", 1);
+}
+
+void VeloPublisher::publishCloud(cv::String fpath)
+{
+    pcl::PointCloud<pcl::PointXYZI> cloud;
+    cloud = KittiReader::getPointcloud(fpath);
+    cloud.header.frame_id = "kitti_frame";
+    _publisher.publish(cloud);
+}
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "kittivelo");
-    ros::NodeHandle nh;
-    ros::Publisher publisher = nh.advertise<pcl::PointCloud<pcl::PointXYZI>>("/velodyne/points", 1);
+    VeloPublisher node = VeloPublisher();
 
     std::string globPattern = std::string(argv[1]) + "velodyne_points/data/*.bin";
     std::vector<cv::String> fpaths = KittiReader::globFilesHelper(globPattern);
 
-    pcl::PointCloud<pcl::PointXYZI> cloud;
     ros::Rate loop_rate(1);
     int count = 0;
     while (ros::ok())
     {
-        cloud = KittiReader::getPointcloud(fpaths[count]);
-        cloud.header.frame_id = "kitti_frame";
-
         ROS_INFO("Publishing pointcloud.");
-        publisher.publish(cloud);
+        node.publishCloud(fpaths[count]);
         ros::spinOnce();
         loop_rate.sleep();
         ++count;
---
+++ src/velopublisher.hpp
@@ -0,0 +1,23 @@
+#ifndef VELOPUBLISHER_HPP
+#define VELOPUBLISHER_HPP
+
+#include <opencv2/opencv.hpp>
+#include "kittireader.hpp"
+#include "ros/ros.h"
+#include <pcl_ros/point_cloud.h>
+#include <pcl/point_types.h>
+
+
+
+class VeloPublisher
+{
+    private:
+        ros::NodeHandle _nh;
+        ros::Publisher _publisher;
+
+    public:
+        VeloPublisher();
+        void publishCloud(cv::String fpath);
+};
+
+#endif
